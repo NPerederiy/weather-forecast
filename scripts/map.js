8fb992687418a668f2fcf26daa2119224f314342
Stranger in the darkness <sergeyscorpjr@gmail.com>
2018-10-17 19:33:50 +0300

    Added hover selection/deselection

--- scripts/map.js
+++ scripts/map.js
@@ -1,6 +1,7 @@
 // showing area description by hover
 $('.area').hover(function() { 
     console.log($(this));
+	$(this).css({stroke-width: 3});
     //$('#description').html($(this).attr('title'));
     //$('#description').fadeIn();
 },
@@ -9,6 +10,14 @@ function(){
 });
 // changing the color of the area by click
 $('.area').click(function() {      
-    $('.area').css({ fill: '#0069cc' });
-    $(this).css({ fill: '#8ecaff' });
+    $('.area').animate(
+	{
+		fill: '#0069cc'
+	}, 500, function () {$('.area').css({fill: '#0069cc'})});
+	//css({ fill: '#0069cc' });
+    $(this).animate(
+	{
+		fill: '#8ecaff'
+	}, 500, function () {$('.area').css({fill: '#8ecaff'})});
+	//css({ fill: '#8ecaff' });
 });
